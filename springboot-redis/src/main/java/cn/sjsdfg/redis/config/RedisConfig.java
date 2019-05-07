bdb8daf0479dc0966ad93d4aebca0c316afbc8b5
sjsdfg <736777445@qq.com>
2019-05-07 22:15:21 +0800

    丰富注释 增加代码实现

--- springboot-redis/src/main/java/cn/sjsdfg/redis/config/RedisConfig.java
+++ springboot-redis/src/main/java/cn/sjsdfg/redis/config/RedisConfig.java
@@ -52,8 +52,11 @@ public class RedisConfig {
                                                        RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(connectionFactory);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // 值采用json序列化
         redisTemplate.setValueSerializer(serializer);
+        // 设置hash key 和value序列化模式
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(serializer);
         return redisTemplate;
@@ -64,7 +67,9 @@ public class RedisConfig {
         final Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         final ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
                 .json().build();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
         objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
         return jackson2JsonRedisSerializer;
